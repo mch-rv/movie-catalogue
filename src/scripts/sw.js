670680bed310f119767fe36aa107bb229beb3957
Mochammad Ravly <mohamadravly@gmail.com>
2023-03-22 14:59:13 +0700

    Initialize movie catalogue automated testing implemented

---
+++ src/scripts/sw.js
@@ -0,0 +1,19 @@
+import { precacheAndRoute } from 'workbox-precaching';
+
+// Do precaching
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('install', () => {
+  console.log('Service Worker: Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('notificationclick', (event) => {
+  const clickedNotification = event.notification;
+  clickedNotification.close();
+  const chainPromise = async () => {
+    console.log('Notification has been clicked');
+    await self.clients.openWindow('https://www.dicoding.com/');
+  };
+  event.waitUntil(chainPromise());
+});
